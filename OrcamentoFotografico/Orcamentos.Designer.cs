0f9c23ab55e27017c3735dafba61129e3c1aa5ba
Tamires <tamirespatrocinioo@gmail.com>
2021-09-21 18:59:46 -0300

    Adicionar arquivos de projeto.

---
+++ OrcamentoFotografico/Orcamentos.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace OrcamentoFotografico
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelPhoto = new System.Windows.Forms.Panel();
+            this.pictureBoxPhoto = new System.Windows.Forms.PictureBox();
+            this.comboBoxOptions = new System.Windows.Forms.ComboBox();
+            this.richTextBoxInfo = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxValor = new System.Windows.Forms.TextBox();
+            this.panelPhoto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(301, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valores Ensaios Fotográficos";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(55, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Selecione uma opção para saber mais:";
+            // 
+            // panelPhoto
+            // 
+            this.panelPhoto.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelPhoto.Controls.Add(this.pictureBoxPhoto);
+            this.panelPhoto.Location = new System.Drawing.Point(516, 106);
+            this.panelPhoto.Name = "panelPhoto";
+            this.panelPhoto.Size = new System.Drawing.Size(377, 496);
+            this.panelPhoto.TabIndex = 2;
+            // 
+            // pictureBoxPhoto
+            // 
+            this.pictureBoxPhoto.Location = new System.Drawing.Point(12, 16);
+            this.pictureBoxPhoto.Name = "pictureBoxPhoto";
+            this.pictureBoxPhoto.Size = new System.Drawing.Size(353, 466);
+            this.pictureBoxPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPhoto.TabIndex = 0;
+            this.pictureBoxPhoto.TabStop = false;
+            // 
+            // comboBoxOptions
+            // 
+            this.comboBoxOptions.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBoxOptions.FormattingEnabled = true;
+            this.comboBoxOptions.Items.AddRange(new object[] {
+            "Grávida",
+            "New Born",
+            "Duo (Grávida + New Born)",
+            "Casal",
+            "Pré-wedding",
+            "Casamento"});
+            this.comboBoxOptions.Location = new System.Drawing.Point(335, 142);
+            this.comboBoxOptions.Name = "comboBoxOptions";
+            this.comboBoxOptions.Size = new System.Drawing.Size(145, 29);
+            this.comboBoxOptions.TabIndex = 3;
+            this.comboBoxOptions.SelectedIndexChanged += new System.EventHandler(this.comboBoxOptions_SelectedIndexChanged);
+            // 
+            // richTextBoxInfo
+            // 
+            this.richTextBoxInfo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBoxInfo.Location = new System.Drawing.Point(55, 198);
+            this.richTextBoxInfo.Name = "richTextBoxInfo";
+            this.richTextBoxInfo.Size = new System.Drawing.Size(425, 304);
+            this.richTextBoxInfo.TabIndex = 5;
+            this.richTextBoxInfo.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(375, 532);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "R$";
+            // 
+            // textBoxValor
+            // 
+            this.textBoxValor.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBoxValor.Location = new System.Drawing.Point(403, 529);
+            this.textBoxValor.Name = "textBoxValor";
+            this.textBoxValor.Size = new System.Drawing.Size(77, 29);
+            this.textBoxValor.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(941, 649);
+            this.Controls.Add(this.textBoxValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBoxInfo);
+            this.Controls.Add(this.comboBoxOptions);
+            this.Controls.Add(this.panelPhoto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panelPhoto.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panelPhoto;
+        private System.Windows.Forms.PictureBox pictureBoxPhoto;
+        private System.Windows.Forms.ComboBox comboBoxOptions;
+        private System.Windows.Forms.RichTextBox richTextBoxInfo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxValor;
+    }
+}
+
---
+++ OrcamentoFotografico/Orcamentos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OrcamentoFotografico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBoxOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string[] info = new string[] {
+                "– Sessão com aproximadamente 60 fotos.\n– 20 fotos digitais, tratadas e entregues via link.\n– 2 trocas de roupa ou 1 figurino.\n– 3 fundos (branco, preto e cinza) ou 1 cenário clássico.\n– Maquiagem.\n– Realizado todos os dias, também aos Sábados e Domingos.",
+                "- Para bebês de 16 a 30 dias de vida.\n– 20 Fotos em alta resolução.\n– 1 Produção (obrigatoriamente no wrap).\n– Fotos com pais e irmãos",
+                "– Sessão da Gestante com aproximadamente 70 fotos.\n– 30 fotos digitais da Gestante.\n– 2 Trocas de roupa e 1 figurino para a Gestante.\n– 3 fundos (branco, preto, cinza) e 1 cenário clássico.\n– Sessão do Recém-nascido no estúdio, 20 fotos, 1 cenários, 2 figurinos.", 
+                "- Locação Externa.\n- Até 2 horas de sessão (ideal fim de tarde).\n- 50 Fotos editadas entregues por link (núvem).\n- 50 fotos reveladas 10x15cm.",
+                "- Sessão de fotos em locação externa.\n- 1:30h de duração.\n- 40 fotos editadas em alta resolução.\n- Maquiagem profissional.\n- 2 trocas de roupas (looks próprios).",
+                "- 2 fotógrafos profissionais.\n- Cobertura do Making Of.\n- Cobertura da Cerimônia.\n- Ensaio Fotográfico logo após cerimônia.\n- Cobertura da Recepção e Jantar.\n- Cobertura da Festa.\n- Tempo aproximado da cobertura: 10 horas.\n- Todas as fotos em alta resolução.\n- 1 Caixinha com Pendrive.\n- 1 porta - retrato de madeira 15 x 21 cm.\n- 2 porta - retratos de madeira 10 x 15 cm.\n- 2 porta - retratos magnéticos 10 x 15 cm.\n- Disponibilização das fotos para acesso online."
+            };
+
+
+            string[] preco = new string[] {
+                "549,00",
+                "900,00",
+                "849,00",
+                "650,00",
+                "1.100,00",
+                "4.800,00",
+            };
+
+            if (comboBoxOptions.Text == "Grávida")
+            {
+                pictureBoxPhoto.ImageLocation = @"C:\Users\Tamires\projetos\img\gravida.jpg";
+                richTextBoxInfo.Text = info[0];
+                textBoxValor.Text = preco[0];
+            }
+            else if (comboBoxOptions.Text == "New Born")
+            {
+                pictureBoxPhoto.ImageLocation = @"C:\Users\Tamires\projetos\img\newborn.jpg";
+                richTextBoxInfo.Text = info[1];
+                textBoxValor.Text = preco[1];
+            }
+            else if (comboBoxOptions.Text == "Duo (Grávida + New Born)")
+            {
+                pictureBoxPhoto.ImageLocation = @"C:\Users\Tamires\projetos\img\duo.jpg";
+                richTextBoxInfo.Text = info[2];
+                textBoxValor.Text = preco[2];
+            }
+            else if (comboBoxOptions.Text == "Casal")
+            {
+                pictureBoxPhoto.ImageLocation = @"C:\Users\Tamires\projetos\img\casal.jpg";
+                richTextBoxInfo.Text = info[3];
+                textBoxValor.Text = preco[3];
+            }
+            else if (comboBoxOptions.Text == "Pré-wedding")
+            {
+                pictureBoxPhoto.ImageLocation = @"C:\Users\Tamires\projetos\img\prewedding.jpg";
+                richTextBoxInfo.Text = info[4];
+                textBoxValor.Text = preco[4];
+            }
+            else if (comboBoxOptions.Text == "Casamento")
+            {
+                pictureBoxPhoto.ImageLocation = @"C:\Users\Tamires\projetos\img\casamento.jpg";
+                richTextBoxInfo.Text = info[5];
+                textBoxValor.Text = preco[5];
+            }
+        }
+    }
+}
